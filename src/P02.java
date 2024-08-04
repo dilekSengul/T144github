41506354208f5269722cfbf3c83c36ab6f9a0b44
dilek sengul <av.dilekylcnn@gmail.com>
2024-08-04 12:29:41 +0300

    dilek

--- src/P02.java
+++ src/P02.java
@@ -1,5 +1,5 @@
 public class P02 {
     public static void main(String[] args) {
-        System.out.println("merhaba dünya");
+        System.out.println("merhaba dünya dilek github kontrol");
     }
 }
--- src/p04.java
+++ src/p04.java
@@ -2,6 +2,7 @@ public class p04 {
     public static void main(String[] args) {
         System.out.println("Merhaba github");
     }
+
 }
 
 
