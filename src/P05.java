1cc072114fe489bbde8c717a2a6ee387739faa7d
dilek sengul <av.dilekylcnn@gmail.com>
2024-08-04 12:37:32 +0300

    5.gün sonu

--- src/P05.java
+++ src/P05.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class P05 {
     public static void main(String[] args) {
         System.out.println("Merhaba github..");
+        Scanner scan= new Scanner(System.in);
+
     }
 }
